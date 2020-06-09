7a66bbf43b459ce23b41bb27fe0f93f40d4c8b01
Fahira Ahmetspahić <fahmetspah1@etf.unsa.ba>
2020-06-09 15:01:41 +0200

    password

--- BloodAlliance/BloodAlliance/Models/Donor.cs
+++ BloodAlliance/BloodAlliance/Models/Donor.cs
@@ -29,6 +29,7 @@ public class Donor
         [Required]
         public string Username { get; set; }
         [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
         [Required]
         public string Email { get; set; }
